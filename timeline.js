58bc6ac4eadb07124e38f48c32d4e7dfd321c5f1
Andrei Marius Hutupas Antoniu <andreimantoniu@yahoo.com>
2019-11-14 20:28:02 +0100

    testing

--- timeline.js
+++ timeline.js
@@ -1,7 +1,7 @@
 const pace = 100;
 const dataLink = "https://timeline-5237.restdb.io/rest/timeline-data";
 
-function getData() {   
+function getData() {
     fetch(dataLink, {
         method: "GET",
         headers: {
@@ -15,37 +15,58 @@ function getData() {
 getData();
 
 function createInitialTimeline(dataCircles) {
-    
+
     const initialTimelineMain = Math.floor(dataCircles.length / 2);
-    
+
     dataCircles.forEach((value, index) => {
+
+        let infoBox = `<div class="infobox ${value.year}">
+        <div>${value.year}</div>
+        <div>${value.genre}</div>
+        <div>${value.origin}</div>
+        </div>`;
+
+        let containerTop = `<div class="container-top"></div>`;
 
         let circle = `<div id=${value.year} class="circle">${value.year}</div>`;
 
         if (index === initialTimelineMain) {
             circle = `<div id=${value.year} class="circle main">${value.year}</div>`;
+            infoBox = `<div class="infobox show ${value.year}">
+            <div>${value.year}</div>
+            <div>${value.genre}</div>
+            <div>${value.origin}</div>
+            </div>`;
         } else if (index === initialTimelineMain - 1) {
             circle = `<div id=${value.year} class="circle prev">${value.year}</div>`;
         } else if (index === initialTimelineMain + 1) {
             circle = `<div id=${value.year} class="circle next">${value.year}</div>`;
         }
 
+        $('#container').append(containerTop);
+
+        $('#container').append(infoBox);
+
         $('.timeline_circles').append(circle);
 
-        $('.circle').get(index).style.left = `${index * pace }px`;
-        
+        $('.circle').get(index).style.left = `${index * pace + 170}px`;
+
     });
 
-    $('.timeline_circles').css( { 
+    $('.timeline_circles').css({
         transition: "transform 1s",
-        transform:  "translateX(" + -200 + "px)" 
+        transform: "translateX(" + -200 + "px)"
     });
-    setTimeout( function() {  $('.timeline_circles').css( { transition: "none" } ) }, 1000 );
+    setTimeout(function () {
+        $('.timeline_circles').css({
+            transition: "none"
+        })
+    }, 1000);
 
 }
 
 $(document).on("click", ".circle", function () {
-    if($(this).hasClass('main')){
+    if ($(this).hasClass('main')) {
         return;
     }
 
@@ -55,47 +76,96 @@ $(document).on("click", ".circle", function () {
     let lastIndexComma2 = newString.lastIndexOf(",");
     const currentValue = parseInt(newString.substring(lastIndexComma2 + 1).trim());
 
-    if($(this).hasClass('prev')){
+    if ($(this).hasClass('prev')) {
         adjustTimeline(currentValue + 100);
         resetMain(this);
     }
 
-    if($(this).next().hasClass('prev')){
+    if ($(this).next().hasClass('prev')) {
         adjustTimeline(currentValue + 200);
         resetMain(this);
     }
 
-    if($(this).next().next().hasClass('prev')){
+    if ($(this).next().next().hasClass('prev')) {
         adjustTimeline(currentValue + 300);
         resetMain(this);
     }
 
-    if($(this).hasClass('next')){
+    if ($(this).next().next().next().hasClass('prev')) {
+        adjustTimeline(currentValue + 400);
+        resetMain(this);
+    }
+
+    if ($(this).next().next().next().next().hasClass('prev')) {
+        adjustTimeline(currentValue + 500);
+        resetMain(this);
+    }
+
+    if ($(this).next().next().next().next().next().hasClass('prev')) {
+        adjustTimeline(currentValue + 600);
+        resetMain(this);
+    }
+
+    if ($(this).next().next().next().next().next().next().hasClass('prev')) {
+        adjustTimeline(currentValue + 700);
+        resetMain(this);
+    }
+
+    if ($(this).hasClass('next')) {
         adjustTimeline(currentValue - 100);
         resetMain(this);
     }
 
-    if($(this).prev().hasClass('next')){
+    if ($(this).prev().hasClass('next')) {
         adjustTimeline(currentValue - 200);
         resetMain(this);
     }
 
-    if($(this).prev().prev().hasClass('next')){
+    if ($(this).prev().prev().hasClass('next')) {
         adjustTimeline(currentValue - 300);
+        resetMain(this);
+    }
+
+    if ($(this).prev().prev().prev().hasClass('next')) {
+        adjustTimeline(currentValue - 400);
+        resetMain(this);
+    }
+
+    if ($(this).prev().prev().prev().prev().hasClass('next')) {
+        adjustTimeline(currentValue - 500);
+        resetMain(this);
+    }
+
+    if ($(this).prev().prev().prev().prev().prev().hasClass('next')) {
+        adjustTimeline(currentValue - 600);
+        resetMain(this);
+    }
+
+    if ($(this).prev().prev().prev().prev().prev().prev().hasClass('next')) {
+        adjustTimeline(currentValue - 700);
         resetMain(this);
     }
 });
 
 
-function adjustTimeline(size){
-    $('.timeline_circles').css( { 
+function adjustTimeline(size) {
+    $('.timeline_circles').css({
         transition: "transform 1s",
-        transform:  "translateX(" + size + "px)" 
+        transform: "translateX(" + size + "px)"
     });
-    setTimeout( function() {  $('.timeline_circles').css( { transition: "none" } ) }, 1000 );
+    setTimeout(function () {
+        $('.timeline_circles').css({
+            transition: "none"
+        })
+    }, 1000);
 }
 
 function resetMain(newMain) {
+
+    $('.infobox').removeClass('show');
+    $(`.${newMain.id}`).addClass('show');
+
+    console.log($(`.${newMain.id}`));
 
     $('.circle').removeClass('main');
     $('.circle').removeClass('prev');
@@ -103,10 +173,10 @@ function resetMain(newMain) {
 
     setTimeout(() => {
         $(newMain).text(newMain.id);
-        if($(newMain).next().length > 0){
+        if ($(newMain).next().length > 0) {
             $(newMain).next().text(newMain.nextSibling.id);
         }
-        if($(newMain).prev().length > 0){
+        if ($(newMain).prev().length > 0) {
             $(newMain).prev().text(newMain.previousSibling.id);
         }
     }, 300);
@@ -116,5 +186,3 @@ function resetMain(newMain) {
     $(newMain).prev().addClass('prev');
 
 }
-
-
